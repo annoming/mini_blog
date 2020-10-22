ffa7d4f201302e1cc8b49483248cfb0a37fb68cd
annoming <annoming_2899@163.com>
2020-10-22 22:51:12 +0800

    添加fail方法，并进行不同的封装

--- src/main/java/com/atming/common/lang/Result.java
+++ src/main/java/com/atming/common/lang/Result.java
@@ -23,6 +23,21 @@ public class Result implements Serializable {
         return success(200,"操作成功",data);
     }
 
+    public static Result fail(String msg) {
+        return fail(400,msg,null);
+    }
+
+    public static Result fail(String msg,Object data) {
+        return fail(400,msg,data);
+    }
+    public static Result fail(int code,String msg,Object data) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        result.setData(data);
+        return result;
+    }
+
     public static Result success(int code, String msg, Object data) {
         Result result = new Result();
         result.setCode(code);
