ff6c5be863b5f1122d45b8b2c86d363c3fa761e0
annoming <annoming_2899@163.com>
2020-10-23 21:13:41 +0800

    修改pom.xml文件的shiro-redis依赖版本,编写ShiroConfig类并重写sessionManager和securityManager方法

---
+++ src/main/java/com/atming/config/ShiroConfig.java
@@ -0,0 +1,58 @@
+package com.atming.config;
+
+import org.apache.shiro.mgt.SessionsSecurityManager;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.RedisSessionDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author: Annoming
+ * @Date: 2020/10/22
+ * @Time: 23:35
+ * @Description
+ */
+@Configuration
+public class ShiroConfig {
+
+
+
+    /*@Autowired
+    RedisSessionDAO redisSessionDAO;
+
+    @Autowired
+    RedisCacheManager redisCacheManager;*/
+
+    @Bean
+    public SessionManager sessionManager(RedisSessionDAO redisSessionDAO) {
+        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+
+        // inject redisSessionDAO
+        sessionManager.setSessionDAO(redisSessionDAO);
+
+        // other stuff...
+
+        return sessionManager;
+    }
+
+    @Bean
+    public SessionsSecurityManager securityManager(List<Realm> realms, SessionManager sessionManager,RedisCacheManager redisCacheManager) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager(realms);
+
+        //inject sessionManager
+        securityManager.setSessionManager(sessionManager);
+
+        // inject redisCacheManager
+        securityManager.setCacheManager(redisCacheManager);
+
+        // other stuff...
+
+        return securityManager;
+    }
+}
