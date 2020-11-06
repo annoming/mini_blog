632b14269d01a41a5f721a28982ef11d73e7b159
annoming <annoming_2899@163.com>
2020-11-06 10:09:33 +0800

    为全局异常捕获类添加实体检验异常方法

--- src/main/java/com/atming/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/atming/common/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.atming.common.lang.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.ShiroException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,6 +28,12 @@ public class GlobalExceptionHandler {
         return Result.fail(401,e.getMessage(),null);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public Result handler(MethodArgumentNotValidException e){
+        log.error("实体校验异常：==============={}",e);
+        return Result.fail(e.getMessage());
+    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = RuntimeException.class )
