00f93c8ee9144120c9695cebd95d73044f4fb800
annoming <annoming_2899@163.com>
2020-11-06 10:52:14 +0800

    添加类LoginDto类操作登录注册需要的字段

---
+++ src/main/java/com/atming/common/dto/LoginDto.java
@@ -0,0 +1,24 @@
+package com.atming.common.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author: Annoming
+ * @Date: 2020/11/6
+ * @Time: 10:34
+ * @Description
+ */
+@Data
+public class LoginDto implements Serializable {
+
+    @NotBlank(message = "昵称不能为空")
+    private String username;
+
+    @NotBlank(message = "密码不能为空")
+    private String password;
+}
